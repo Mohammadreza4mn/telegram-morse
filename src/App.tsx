0231ea0c42675ebd978b9c0aaba4fcd7cdcb5ddf
MohammadReza <md.mohammadreza+git@gmail.com>
2024-06-19 22:51:24 +0330

    add theme and telegram provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { useState } from "react";
-import { ThemeProvider } from "styled-components";
 import Home from "pages/Home";
 import GlobalStyles from "styles/global";
-import {
-  darkTheme,
-  defaultColorThemeName,
-  lightTheme,
-  type ThemeName,
-} from "theme";
+import TelegramProvider from "context/telegram";
+import ThemeProvider from "context/theme";
 
 function App() {
-  const [mode, _] = useState<ThemeName>(defaultColorThemeName);
-  const theme = mode === "light" ? lightTheme : darkTheme;
-
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Home />
-    </ThemeProvider>
+    <TelegramProvider>
+      <ThemeProvider>
+        <GlobalStyles />
+        <Home />
+      </ThemeProvider>
+    </TelegramProvider>
   );
 }
 
