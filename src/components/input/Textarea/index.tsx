5bb794b85e1632d81ef92ed05bac2751dc90313a
MohammadReza <md.mohammadreza+git@gmail.com>
2024-06-19 22:50:47 +0330

    remove default row

--- src/components/input/Textarea/index.tsx
+++ src/components/input/Textarea/index.tsx
@@ -2,7 +2,7 @@ import { TextareaHTMLAttributes } from "react";
 import { StyledTextarea } from "./styled";
 
 function Textarea(props: TextareaHTMLAttributes<HTMLTextAreaElement>) {
-  return <StyledTextarea rows={10} {...props} />;
+  return <StyledTextarea {...props} />;
 }
 
 export default Textarea;
