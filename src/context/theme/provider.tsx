acbc0d13f8f26649477022a07d1f7a67b73ba144
MohammadReza <md.mohammadreza+git@gmail.com>
2024-06-21 02:12:00 +0330

    remove color schema

--- src/context/theme/provider.tsx
+++ src/context/theme/provider.tsx
@@ -1,13 +1,8 @@
-import { useTelegram } from "context/telegram";
 import { FC, ReactNode } from "react";
-import { darkTheme, lightTheme } from "theme";
 import { ThemeProvider as ThemeStyled } from "styled-components";
+import { theme } from "theme";
 
 const ThemeProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const { colorScheme } = useTelegram();
-
-  const theme = colorScheme === "light" ? lightTheme : darkTheme;
-
   return <ThemeStyled theme={theme}>{children}</ThemeStyled>;
 };
 
