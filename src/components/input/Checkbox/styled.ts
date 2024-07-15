3f51c29d478a7f4d7650a9d3f18feaf6b12fbe84
MohammadReza <md.mohammadreza+git@gmail.com>
2024-07-15 17:26:04 +0330

    update style checkbox

--- src/components/input/Checkbox/styled.ts
+++ src/components/input/Checkbox/styled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const SLabel = styled.label`
   font-size: 1rem;
+  user-select: none;
   text-align: center;
   color: ${({ theme }) => theme.colors.primaryText};
   cursor: pointer;
