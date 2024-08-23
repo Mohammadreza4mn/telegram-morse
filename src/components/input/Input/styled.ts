5132597eb191bbabb985fe8333f9391f2ceb9f36
MohammadReza <md.mohammadreza+git@gmail.com>
2024-08-23 15:59:00 +0330

    add background color input

--- src/components/input/Input/styled.ts
+++ src/components/input/Input/styled.ts
@@ -8,6 +8,10 @@ const StyledInput = styled.input`
   border: ${({ theme }) => `0.063rem solid ${theme.colors.border}`};
   padding: 0.5rem 0.9rem;
   color: ${({ theme }) => theme.colors.primaryText};
+  background-color: ${({ theme }) => theme.colors.primaryBg};
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.primaryText};
+  }
 `;
 
 export { StyledInput };
