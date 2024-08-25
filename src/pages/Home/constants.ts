86d7e1b47943a940bd187405444cde9c8df2afd2
MohammadReza <md.mohammadreza+git@gmail.com>
2024-08-25 21:51:58 +0330

    update constant

--- src/pages/Home/constants.ts
+++ src/pages/Home/constants.ts
@@ -4,14 +4,17 @@ const translateDefaultMode: TTranslateMode = "textToMorse";
 const copyrightMessage =
   "Add the bot's address to the end of the message when you copy it.";
 const messageMode = { checked: "private", unchecked: "public" };
-const secretKey = "-.-.-.-.";
+const publicKey = "-.-.-.-.";
+const robotCopyright =
+  "🤖translated by https://t.me/morse_code_translator_bot/start";
 const confirmPrivateMessages = `This message is confidential, if you are the recipient, select Ok and know that we need your phone number for decoding, and if you are not the recipient, please select Cancel.
 Note: Your phone number is not stored anywhere.`;
 
 export {
-  translateDefaultMode,
-  copyrightMessage,
+  publicKey,
   messageMode,
-  secretKey,
+  robotCopyright,
+  copyrightMessage,
+  translateDefaultMode,
   confirmPrivateMessages,
 };
