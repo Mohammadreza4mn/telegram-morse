1f5afa5a59e6c8604f2650f236441d01f60ebd73
MohammadReza <md.mohammadreza+git@gmail.com>
2024-09-13 17:50:48 +0330

    add timer key

--- src/pages/Home/constants.ts
+++ src/pages/Home/constants.ts
@@ -1,20 +1,19 @@
 import { TTranslateMode } from "./interface";
 
 const translateDefaultMode: TTranslateMode = "textToMorse";
-const copyrightMessage =
-  "Add the bot's address to the end of the message when you copy it.";
 const messageMode = { checked: "private", unchecked: "public" };
-const publicKey = "-.-.-.-.";
+const recipientKey = process.env.REACT_APP_RECIPIENT_KEY as string;
+const timerKey = process.env.REACT_APP_TIMER_KEY as string;
 const robotCopyright =
   "🤖translated by https://t.me/morse_code_translator_bot/start";
 const confirmPrivateMessages = `This message is confidential, if you are the recipient, select Ok and know that we need your phone number for decoding, and if you are not the recipient, please select Cancel.
 Note: Your phone number is not stored anywhere.`;
 
 export {
-  publicKey,
+  timerKey,
   messageMode,
+  recipientKey,
   robotCopyright,
-  copyrightMessage,
   translateDefaultMode,
   confirmPrivateMessages,
 };
