600e0b5b0e7cf3937bdcb0502569ea22f0112cb8
MohammadReza <md.mohammadreza+git@gmail.com>
2024-10-25 20:11:01 +0330

    remove switch cmp

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ export { default as Button } from "./Button";
 export { default as Spinner } from "./Spinner";
 export { default as Textarea } from "./input/Textarea";
 export { default as Checkbox } from "./input/Checkbox";
-export { default as Switch } from "./input/Switch";
 export { default as Input } from "./input/Input";
 export { default as DateTime } from "./input/DateTime";
